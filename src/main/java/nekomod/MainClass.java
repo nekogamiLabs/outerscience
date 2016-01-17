1a438f36fbcf0f672bac551e842ab6049d4551f6
ScriptKittyFN <sciencewez@gmail.com>
2016-01-16 20:18:20 -0500

    added comments

--- src/main/java/nekomod/MainClass.java
+++ src/main/java/nekomod/MainClass.java
@@ -1,5 +1,6 @@
 package nekomod;
 
+//these are the imports, the code this code refers to.
 import nekomod.fluids.FluidLcl;
 import nekomod.items.ItemDNA;
 import nekomod.items.LclBucket;
@@ -27,32 +28,41 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-@Mod(modid="outerscience", name="OuterScience", version="1.1")
+@Mod(modid="outerscience", name="OuterScience", version="1.1") //sets mod info
 
 public class MainClass
 {
-	
-  @SidedProxy(clientSide="nekomod.ClientProxy", serverSide="nekomod.CommonProxy")
+  @SidedProxy(clientSide="nekomod.ClientProxy", serverSide="nekomod.CommonProxy") //points to proxys
+  //public - all classes can see this, static - can not be changed, CommonProxy - what it points to, proxy - what you put in the code to point to it.
   public static CommonProxy proxy;
+  //modid
   public static final String modid = "outerscience";
+  //Block variables
   public static Block bioChamber;
   public static Block bioChamberA;
-  public static Item dna;
-  public static CreativeTabs tabMyMod = new CreativeTabOuter("OuterScience");
-  public static Fluid lcl;
-  
-  @Instance(modid)
-  public static MainClass modInstance;
+  //fluidBlock
   public static Block yourFluidBlock;
+  //Items
+  public static Item dna;
   public static Item syringe;
   public static Item sterilesyringe;
   public static Item lclBucket;
+  //Other
+  public static CreativeTabs tabMyMod = new CreativeTabOuter("OuterScience");
+  public static Fluid lcl;
+  
+  @Instance(modid)//many things need this instance 
+  public static MainClass modInstance;
+  
+  
+  
   
   public static void registerEntity(){
-
+	  	//registers entities, use createEntity function here
 		createEntity(EntityNeko.class, "NekoGirl", 0x000000, 0xD707DE, BiomeGenBase.forest, BiomeGenBase.birchForest, BiomeGenBase.roofedForest, 40, 2, 6);
 		
 	}
+  	//function for creating new entities DO NOT CHANGE
 	public static void createEntity(Class entityClass, String entityName, int solidColor, int spotColor, BiomeGenBase biome1, BiomeGenBase biome2, BiomeGenBase biome3, int spawnProb, int minSpawn, int  maxSpawn){
 		int randomID = EntityRegistry.findGlobalUniqueEntityId();
 		EntityRegistry.registerGlobalEntityID(entityClass, entityName, randomID);
@@ -61,31 +71,35 @@ public class MainClass
 		
 		createEgg(randomID, solidColor, spotColor);
 	}
-	
+	//create egg function DO NOT CHANGE
 	private static void createEgg(int randomID, int solidColor, int spotColor){
 		EntityList.entityEggs.put(Integer.valueOf(randomID), new EntityList.EntityEggInfo(randomID, solidColor, spotColor));
 	}
 
+	
+  //MAIN EVENT
   @Mod.EventHandler
   public void load(FMLInitializationEvent event)
   {
+	//proxy functions
     proxy.registerRenderInformation();
     proxy.init(event);
     proxy.registerTileEntities();
     
     
     
-    registerBlocks();
+    registerBlocks(); //registers blocks
     
-    registerEntity();
+    registerEntity(); //registers entities 
     
-    registerRecipies();
+    registerRecipies(); //registers crafting
     
-    registerItems();
+    registerItems(); //registers items
     
     }
   
-  private void registerItems() {
+  private void registerItems() { //ALL ITEMS GO HERE
+	  //DNA item
 	  dna = new ItemDNA().setUnlocalizedName("DNA").setTextureName("outerscience:dna");
 	  GameRegistry.registerItem(dna, dna.getUnlocalizedName().substring(5));
 	  
@@ -102,7 +116,8 @@ public class MainClass
 			);
   }
   
-  private void registerBlocks() {
+  private void registerBlocks() { //ALL BLOCKS GO HERE
+	//FLUID BLOCKS
 	lcl = new Fluid("fluidname").setIcons(FluidLcl.flowingIcon).setLuminosity(5).setDensity(90).setViscosity(10);
 	FluidRegistry.registerFluid(lcl);
 	yourFluidBlock = new FluidLcl(lcl, Material.water).setBlockName("fluidlcl");
@@ -110,10 +125,12 @@ public class MainClass
 	lcl.setUnlocalizedName(yourFluidBlock.getUnlocalizedName());
   }
   
-  private void registerRecipies(){
+  private void registerRecipies(){ //crafting and crafting variables go here
+	//Var  
 	ItemStack dnaI = new ItemStack(dna);
 	FluidStack lclF = new FluidStack(lcl, 100);
 	  
-	//cofh.api.modhelpers.ThermalExpansionHelper.addCrucibleRecipe(4000, dnaI, lclF);
+	//note the lib must be in build path for this error to be resolved DO NOT CHANGE
+	cofh.api.modhelpers.ThermalExpansionHelper.addCrucibleRecipe(4000, dnaI, lclF);
   }
 }
